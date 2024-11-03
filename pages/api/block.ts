c43a956b7a25c29ed87e657030b7133b872701b0
Danil Zdoryk <danil.zdoryk@student.tuke.sk>
2024-11-03 16:22:00 +0100

    added changes, also have problem with Tags, bad state

--- pages/api/block.ts
+++ pages/api/block.ts
@@ -3,6 +3,6 @@ import { NextResponse } from 'next/server';
 export default function handler(req:any, res:any) {
     // Проверяем метод запроса (GET, POST и т.д.)
     if (req.method === 'GET') {
-      res.status(200).json({ message: true});
+      res.status(200).json({ message: false});
     }
   }
