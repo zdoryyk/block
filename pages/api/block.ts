a9ee4513b78ad94f2458ba07bde32b359a01a607
Danil Zdoryk <danil.zdoryk@student.tuke.sk>
2024-11-03 16:39:17 +0100

    added changes, also have problem with Tags, bad state

--- pages/api/block.ts
+++ pages/api/block.ts
@@ -1,8 +1,15 @@
 import { NextResponse } from 'next/server';
 
+let isBLocked = false;
+
 export default function handler(req:any, res:any) {
     // Проверяем метод запроса (GET, POST и т.д.)
     if (req.method === 'GET') {
-      res.status(200).json({ message: false});
+      res.status(200).json({ message: isBLocked});
     }
+    if(req.method === 'POST'){
+      isBLocked = !isBLocked;
+      res.status(200).json({ message: 'Is blocked = ' + isBLocked});
+    }
+    
   }
